8dbfb2a5f60ef0028cf771188197dfc5dc8c43a2
Máté Donka <mate.salamon@gmail.com>
2021-06-08 21:55:41 +0200

    test: fix MovieBox test

--- src/components/MovieBox/MovieBox.test.tsx
+++ src/components/MovieBox/MovieBox.test.tsx
@@ -4,21 +4,31 @@ import { render, screen } from '@testing-library/react'
 import MovieBox from './MovieBox'
 import { mapGenres } from './helpers'
 import movie from '../../mocks/movie'
+import { ThemeProvider } from 'styled-components'
+import theme from '../../theme'
 
 describe('MovieBox', () => {
   beforeEach(() => {
-    render(<MovieBox movie={movie} />)
+    render(
+      <ThemeProvider theme={theme}>
+        <MovieBox movie={movie} />
+      </ThemeProvider>,
+    )
   })
 
-  it('renders image', () => {
+  it('displays image', () => {
     expect(screen.getByRole('img').getAttribute('src')).toBe(movie.poster.medium)
   })
 
-  it('renders name', () => {
+  it('displays score', () => {
+    expect(screen.getByText(movie.score)).toBeDefined()
+  })
+
+  it('displays name', () => {
     expect(screen.getByText(movie.name)).toBeDefined()
   })
 
-  it('renders genres', () => {
+  it('displays genres', () => {
     expect(screen.getByText(mapGenres(movie.genres))).toBeDefined()
   })
 })
