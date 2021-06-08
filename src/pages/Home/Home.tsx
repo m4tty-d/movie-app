f6de2be7764c783647b9928a477576f4bc82928d
Máté Donka <mate.salamon@gmail.com>
2021-06-09 00:32:17 +0200

    feat: use search movies query

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,20 @@
 import React, { Fragment } from 'react'
-import { Container, Box, Typography } from '@material-ui/core'
-import TopBar from '../../components/TopBar'
+import { Container, Box, Typography, CircularProgress } from '@material-ui/core'
+import TopBar from '../../components/TopBar/'
 import MovieList from '../../components/MovieList'
 import { Holder } from './HomeStyles'
+import { useSearchMoviesQuery } from '../../__generated__/graphql'
+import { useRecoilState } from 'recoil'
+import { searchQueryState } from '../../store'
 
-import movie from '../../mocks/movie'
-const movies = Array.from(Array(10).keys()).map((i) => ({ ...movie, id: `${movie.id}-${i}` }))
+const Home: React.FC = () => {
+  const [searchQuery] = useRecoilState(searchQueryState)
+  const { data, loading } = useSearchMoviesQuery({
+    variables: { query: searchQuery },
+    skip: !searchQuery || !searchQuery.length,
+  })
+  const movies = data?.searchMovies || []
 
-function App() {
   return (
     <Fragment>
       <TopBar />
@@ -15,14 +22,14 @@ function App() {
         <Container>
           <Box mb={4}>
             <Typography variant="h4" component="h1">
-              Results for
+              {movies.length || loading ? `Results for "${searchQuery}"` : `Search to get started! :)`}
             </Typography>
           </Box>
-          <MovieList movies={movies} />
+          {loading ? <CircularProgress /> : <MovieList movies={movies} />}
         </Container>
       </Holder>
     </Fragment>
   )
 }
 
-export default App
+export default Home
