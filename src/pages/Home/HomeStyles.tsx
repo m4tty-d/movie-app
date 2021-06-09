ecaa3bef2b575cef0b12eabd3f1a87aa32b8b7f3
Máté Donka <mate.salamon@gmail.com>
2021-06-09 21:04:40 +0200

    fix: Home page height

--- src/pages/Home/HomeStyles.tsx
+++ src/pages/Home/HomeStyles.tsx
@@ -3,7 +3,7 @@ import { Box, withStyles } from '@material-ui/core'
 export const Holder = withStyles((theme) => ({
   root: {
     background: 'radial-gradient(circle, rgba(47,60,77,1) 0%, rgba(32,33,38,1) 100%)',
-    minHeight: '100vh',
+    minHeight: 'calc(100vh - 64px)',
     width: '100vw',
     padding: theme.spacing(4),
   },
